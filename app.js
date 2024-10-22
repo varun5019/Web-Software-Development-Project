f3c309b432719af6562a5f71bfbac905bd36c220
Varun Sangwan <74721898+varun5019@users.noreply.github.com>
2024-10-22 04:20:08 +0300

    Update app.js

--- app.js
+++ app.js
@@ -2,6 +2,29 @@ import { Hono } from "https://deno.land/x/hono@v3.12.11/mod.ts";
 
 const app = new Hono();
 
-app.get("/", (c) => c.text("Hi again!"));
+let count = 0;
+let answer = 0;
 
+app.get("/", (c) => c.text(count));
+
+app.post("/", (c) => {
+  count++;
+  if (count === 1)
+  {
+    answer = '3,'; 
+  }
+  else if (count === 2)
+  {
+      answer = '2,';
+  }
+  else if (count === 3)
+  {
+      answer = '1,';
+  }
+  else
+  {
+      answer = 'Kaboom!';
+  }
+  return c.text(answer);
+});
 export default app;
