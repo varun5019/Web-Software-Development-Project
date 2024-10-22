3beb50e7d51c58a9436fe2b2b8542c7c87790813
Varun Sangwan <74721898+varun5019@users.noreply.github.com>
2024-10-22 04:04:19 +0300

    Update app.js

--- app.js
+++ app.js
@@ -2,6 +2,6 @@ import { Hono } from "https://deno.land/x/hono@v3.12.11/mod.ts";
 
 const app = new Hono();
 
-app.get("/", (c) => c.text("Hello World!"));
+app.get("/", (c) => c.text("Hi there!"));
 
 export default app;
